d7eac0e6c3521d963bf7c7bf94e834779ac7b102
fitedpro <fitedpro@gmail.com>
2018-06-26 14:52:46 +0300

    Change structure project. Connect to DataBase Add CRUD.

--- src/main/java/com/fitedpro/shop/repos/UserRepo.java
+++
@@ -1,8 +0,0 @@
-package com.fitedpro.shop.repos;
-
-import com.fitedpro.shop.domain.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepo extends CrudRepository<User, Long> {
-
-}
